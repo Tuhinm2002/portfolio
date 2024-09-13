45cb394de4b842e56b05ef2ca8a1c025ad99b4f6
unknown <tuhinm2002@gmail.com>
2024-09-13 23:05:46 +0530

    data props added

--- src/components/ProjectsView.jsx
+++ src/components/ProjectsView.jsx
@@ -21,7 +21,39 @@ const icon = (
 
 const projectData = [
   {
-    
+    projectName : "cars-commerce",
+    githubLink : 'https://github.com/Tuhinm2002/cars-commerce.git',
+    about : "* Developed a full-stack ecommerce platform using Java Spring (backend) and React (frontend)" +
+            "* Implemented dynamic product listings with an intuitive user interface." + 
+            "* Technologies used: JavaScript (64.0%), Java (27.5%), CSS (6.4%), and HTML (2.1%)" +
+            "* Focused on performance, scalability, and user experience.",
+    deployLink : ''
+  },
+  {
+    projectName : "Bert YouTube Sentiment",
+    githubLink : "https://github.com/Tuhinm2002/bert_youtube_sentiment.git",
+    about : "* This project is designed to analyze sentiment in comments from any uploaded YouTube video." +
+            "* It focuses on categorizing comments into two main categories: positive and negative sentiments." + 
+            "* Utilizes a fine-tuned model trained on a comprehensive Twitter comments dataset." +
+            "* Deployed on a server environment using advanced technologies like Hugging Face Spaces and Docker." ,
+    deployLink : "https://huggingface.co/spaces/Tuhinm2002/bert_youtube_sentiment"
+  },
+  {
+    projectName : "BERTVision",
+    githubLink : "https://github.com/Tuhinm2002/BERTVision.git",
+    about : "* This project aims to fuse the realms of Computer Vision and Natural Language Processing." +
+                  "* Utilizing a BERT model, the project discerns emotions from text in real time via computer vision." +
+                  "* Display text before a camera to enable the web app to instantaneously predict and categorize the text." +
+                  "* Deploying via Hugging Face Spaces, and packaging with Docker ensure streamlined management and deployment.",
+    deployLink : "https://huggingface.co/spaces/Tuhinm2002/bert-vision"
+  },
+  {
+    projectName : "Frontend community website",
+    githubLink : "https://github.com/Tuhinm2002/website.git",
+    about : "* Developed a front-end project using React JS, HTML, and Tailwind CSS for a Twitter-based software engineering community." +
+            "* Ensured project quality through CI/CD pipelines for continuous integration and delivery."+
+            "* Contributed to open-source development, fostering community engagement and collaboration.",
+    deployLink : "https://tuhinm2002.github.io/website/"
   }
 ]
 
@@ -47,15 +79,23 @@ export default function ProjectsView() {
         }}
       >
         <Box>
-          <Box><MediaCard/></Box>
-          <Box><MediaCard/></Box>
+          <Box><MediaCard projectName = {projectData[0].projectName} 
+          githubLink = {projectData[0].githubLink} about = {projectData[0].about}
+          deployLink = {projectData[0].deployLink}/></Box>
+          <Box><MediaCard projectName = {projectData[1].projectName} 
+          githubLink = {projectData[1].githubLink} about = {projectData[1].about}
+          deployLink = {projectData[1].deployLink}/></Box>
         </Box>
         <Box>
           <Box sx={{ width: '100%',marginTop:'15%' }}>
-            <MediaCard/>
+            <MediaCard projectName = {projectData[2].projectName} 
+          githubLink = {projectData[2].githubLink} about = {projectData[2].about}
+          deployLink = {projectData[2].deployLink}/>
           </Box>
           <Box sx={{ width: '100%',marginTop:'15%' }}>
-              <MediaCard/>
+              <MediaCard projectName = {projectData[3].projectName} 
+          githubLink = {projectData[3].githubLink} about = {projectData[3].about}
+          deployLink = {projectData[3].deployLink}/>
           </Box>
         </Box>
       </Box>
